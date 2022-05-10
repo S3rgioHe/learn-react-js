11b66fdaf0064ed97154ac6d8394e65bad2aab9b
S3rgioHe <sergioextremehe@gmail.com>
2022-05-10 16:57:28 -0300

    feat: add style's loaders

--- ignite-aula/reactjs/01-github-explorer/src/App.jsx
+++ ignite-aula/reactjs/01-github-explorer/src/App.jsx
@@ -1,7 +1,5 @@
 export function App() {
 
-    throw new Error('eita giovana, o forninho caiu!')
-
     return <div className="Teste">
         <h1>Hello React</h1>
     </div>
--- ignite-aula/reactjs/01-github-explorer/src/index.jsx
+++ ignite-aula/reactjs/01-github-explorer/src/index.jsx
@@ -1,4 +1,5 @@
 import { render } from 'react-dom';
 import { App } from './App'; 
+import './style/global.css';
 
 render(<App />, document.getElementById('root'));
