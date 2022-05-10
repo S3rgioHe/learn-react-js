91d3ce7ec0e21f766430f1158b6f97cc0ab004f0
S3rgioHe <sergioextremehe@gmail.com>
2022-05-10 16:25:18 -0300

    feat: add webpack serve and yours configs

--- ignite-aula/reactjs/01-github-explorer/src/App.jsx
+++ ignite-aula/reactjs/01-github-explorer/src/App.jsx
@@ -1,4 +1,8 @@
-export function App()
-{
-    return <h1>Hello World</h1>
+export function App() {
+
+    throw new Error('eita giovana, o forninho caiu!')
+
+    return <div className="Teste">
+        <h1>Hello React</h1>
+    </div>
 }
--- ignite-aula/reactjs/01-github-explorer/webpack.config.js
+++ ignite-aula/reactjs/01-github-explorer/webpack.config.js
@@ -1,15 +1,23 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
+    devtool: 'eval-source-map',
     entry: path.resolve(__dirname, 'src', 'index'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'build.js',
-      },
-
+    },
     resolve: {
         extensions: ['.js', '.jsx']
     },
+    devServer: {
+        static: path.resolve(__dirname, 'public'),
+    },
+    plugins: [
+        new HtmlWebPackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') })
+    ],
     module: {
         rules: [
             {
