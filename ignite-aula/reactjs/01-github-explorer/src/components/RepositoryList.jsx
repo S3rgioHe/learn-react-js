6e37151fe19e5b63a9cbe647a56283865240e89b
S3rgioHe <sergioextremehe@gmail.com>
2022-05-16 11:23:10 -0300

    test: learn parameters pase in component

--- ignite-aula/reactjs/01-github-explorer/src/components/RepositoryList.jsx
+++ ignite-aula/reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,16 +1,21 @@
-const repositoryName = 'Lista de repositórios';
 import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+    name: "Github Explorer",
+    description: "Um app para explorar repositórios no Github",
+    link: "https://www.youtube.com/watch?v=DYed5whEf4g"
+}
 
 export function RepositoryList() {
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
             </ul>
-                <RepositoryItem repository="unform 2"/>
-                <RepositoryItem repository="teste" />
-                <RepositoryItem repository="aqui"/>
-                <RepositoryItem />
         </section>
     );
 }
