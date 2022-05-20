4582bcc2690e14762849d39b143aa94f584d14bb
S3rgioHe <sergioextremehe@gmail.com>
2022-05-20 12:03:37 -0300

    feat: import style of repositories

--- ignite-aula/reactjs/01-github-explorer/src/components/RepositoryList.jsx
+++ ignite-aula/reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,9 +1,18 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+import '../style/repositories.scss';
+
 const repository = {
     name: "Github Explorer",
     description: "Um app para explorar repositórios no Github",
     link: "https://www.youtube.com/watch?v=DYed5whEf4g"
+}
+
+const teste = {
+    name: "Github Explorer v2",
+    description: "Um app para explorar repositórios no Github",
+    link: "https://www.youtube.com/watch?v=DYed5whEf4g"
+
 }
 
 export function RepositoryList() {
@@ -12,7 +21,7 @@ export function RepositoryList() {
             <h1>Lista de repositórios</h1>
             <ul>
                 <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={teste}/>
                 <RepositoryItem repository={repository}/>
                 <RepositoryItem repository={repository}/>
             </ul>
