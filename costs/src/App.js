f71ed790bb41da75f08a2ee0ca7cf3b85e172583
S3rgioHe <54727461+S3rgioHe@users.noreply.github.com>
2022-05-22 16:36:47 -0300

    class #03

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 
 function App() {
+
+  const name = 'Sérgio';
+  const url = 'https://via.placeholder.com/150';
+
+  function sum(a, b) {
+    return a + b;
+  }
+
   return (
     <div className="App">
-      <h1>Olá React</h1>
-      <p>Estou programando em react</p>
-    </div> 
+      <h1>Alterando o jsx</h1>
+      <p>Olá, {name}</p>
+      <p>{sum(1, 2)}</p>
+      <img src={url} alt="teste de imagem" style={{ "borderRadius": "25px"}}/>
+    </div>
   );
 }
 
