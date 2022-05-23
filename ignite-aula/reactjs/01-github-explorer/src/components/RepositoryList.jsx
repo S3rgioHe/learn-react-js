7ec6e00bc0b53dbce984aa4c8f14f001a5f8319f
S3rgioHe <sergioextremehe@gmail.com>
2022-05-23 16:02:25 -0300

    class list repositories

---
+++ ignite-aula/reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,30 @@
+import { RepositoryItem } from "./RepositoryItem";
+import React, { useState, useEffect } from "react";
+
+import '../style/repositories.scss';
+
+const urlApi = 'https://api.github.com/orgs/rocketseat/repos';
+
+export function RepositoryList() {
+
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch(urlApi)
+        .then(response => response.json())
+        .then(data => setRepositories(data));
+    }, []);
+
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+            <h2>teste</h2>
+            <ul>
+                { repositories.map(repository => {
+                    return <RepositoryItem repository={repository}/>
+                }) }
+
+            </ul>
+        </section>
+    );
+}
