06d8f86786bb5d4a2b4f79e09f119290d8a99aa5
S3rgioHe <sergioextremehe@gmail.com>
2022-05-30 17:08:56 -0300

    class: componente: header

--- ignite-aula/reactjs/dtmoney/src/App.tsx
+++ ignite-aula/reactjs/dtmoney/src/App.tsx
@@ -1,14 +1,11 @@
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  color: #8257e6;
-  font-size: 64px;
-`;
+import { GlobalStyle } from '../src/styles/global';
+import { Header } from './components/Header';
 
 export function App() {
   return (
-    <div className="App">
-      <Title className='title'>Hello World</Title>
-    </div>
+      <>
+        <Header />
+        <GlobalStyle />
+      </>
   );
 }
