b24f919ad29c9970d136a173f5449841abffb543
S3rgioHe <sergioextremehe@gmail.com>
2022-06-02 16:06:17 -0300

    class: configurando modal de criação

--- ignite-aula/reactjs/dtmoney/src/App.tsx
+++ ignite-aula/reactjs/dtmoney/src/App.tsx
@@ -1,13 +1,37 @@
 import { GlobalStyle } from '../src/styles/global';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { useState } from 'react';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root')
+
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModalOpen() {
+      setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModalOpen() {
+      setIsNewTransactionModalOpen(false);
+  }
+  
   return (
-      <>
-        <Header />
-        <Dashboard />
-        <GlobalStyle />
-      </>
+    <>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModalOpen}/>
+      <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModalOpen}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+
+      <GlobalStyle />
+    </>
   );
 }
