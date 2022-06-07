c0e10dcad7df4a35dacf6787bfead7984fb34d18
S3rgioHe <sergioextremehe@gmail.com>
2022-06-07 16:49:29 -0300

    class: estilizando modal

--- ignite-aula/reactjs/dtmoney/src/styles/global.ts
+++ ignite-aula/reactjs/dtmoney/src/styles/global.ts
@@ -80,6 +80,20 @@ export const GlobalStyle = createGlobalStyle`
         opacity: 0.6;
         cursor: not-allowed;
     }
+
+    .react-modal-close {
+        position: absolute;
+        right: 1.5rem;
+        top: 1.5rem;
+        border: 0;
+        background: transparent;
+
+        transition: 0.2s;
+
+        &:hover {
+            filter: brightness(0.5);
+        }
+    }
  `;
 
 
