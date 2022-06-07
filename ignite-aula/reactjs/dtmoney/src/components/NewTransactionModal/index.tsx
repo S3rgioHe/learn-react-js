b37eb62d0b3189a7bc9052ac0fa23091b5d9f4a3
S3rgioHe <sergioextremehe@gmail.com>
2022-06-07 15:39:57 -0300

    class: estrutura do formulário

--- ignite-aula/reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
+++ ignite-aula/reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './style';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -10,8 +11,25 @@ export function NewTransactionModal({ onRequestClose, isOpen }: NewTransactionMo
         <Modal
             isOpen={isOpen}
             onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
         >
+        <Container>
             <h2>Cadastrar transação</h2>
+            <input
+                placeholder='Título'
+            />
+            <input
+                type="number"
+                placeholder="Valor"
+            />
+            <input
+                placeholder='categoria'
+            />
+
+            <button type='submit'>Cadastrar</button>
+            
+        </Container>
         </Modal>
     )
 }
